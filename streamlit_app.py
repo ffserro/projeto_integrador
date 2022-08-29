18f42fb2923e08bbdd3e94ad7d2834b2d64cfac8
ffserro <ffserro@hotmail.com>
2022-08-29 17:10:16 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 st.markdown('# Digital House <img src="https://scontent.fsdu13-1.fna.fbcdn.net/v/t39.30808-6/275930420_1550645198655229_6570428577173776959_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=f28DmQAoRYEAX-DYGY0&_nc_ht=scontent.fsdu13-1.fna&oh=00_AT_G6wfELkAt7dyckIihVED_xXU3vF_iPffZPOZy-LkJxg&oe=6312AE5D" alt="Digital House" style="display:inline-block;margin-left:auto;margin-right:auto;width:10%">', unsafe_allow_html=True)
 
-st.write('# Digital House')
 st.write('## Projeto integrador II')
 st.write('### Eloá Bastos, Felipe Carvalho, Felipe Sêrro, Maiara Firmo, Pedro Aranha e Rafael Mello')
 st.write('O propósito deste app é mostrar como um modelo de regressão treinado pode ser divulgado de forma que facilite o entendimento e torne os resultados mais palpáveis para o tomador de decisões.')
