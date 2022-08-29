e371d3019443d154d51dcefe294b04797fb96454
ffserro <ffserro@hotmail.com>
2022-08-29 16:55:27 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import lightgbm as lgb
 import matplotlib.pyplot as plt
 
-st.title('Projeto integrador')
+st.write('# Digital House')
+st.write('## Projeto integrador II')
+st.write('### Eloá Bastos, Felipe Carvalho, Felipe Sêrro, Maiara Firmo, Pedro Aranha e Rafael Mello')
+st.write('O propósito deste app é mostrar como um modelo de regressão treinado pode ser divulgado de forma que facilite o entendimento e torne os resultados mais palpáveis para o tomador de decisões.')
+st.write('##')
+st.write('')
 
 lgb_model = lgb.Booster(model_file='./LightGBMModel.txt')
 
