72c6acf6ce8aa062801b8c47633eade689cb4089
ffserro <ffserro@hotmail.com>
2022-08-29 16:40:48 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import lightgbm as lgb
 
 st.title('Projeto integrador')
 
-lgb_model = lgb.Booster(model_file='./mode.txt')
+lgb_model = lgb.Booster(model_file='./LightGBMModel.txt')
 
 st.pyplot(lgb_model.plot_importance)
