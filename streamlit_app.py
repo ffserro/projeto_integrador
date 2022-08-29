ab86c60c14801fe999726fc05ce7e8508b453737
ffserro <ffserro@hotmail.com>
2022-08-29 19:14:13 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -340,160 +340,59 @@ with st.form('enem_survey'):
 'TP_ESCOLA': tipo_escola,
 	
 
-'IN_TREINEIRO': treineiro
+'IN_TREINEIRO': treineiro,
+	
+'TP_LINGUA': tipo_lingua, 
 
-TP_DEPENDENCIA_ADM_ESC	Dependência administrativa (Escola)
-	
-	
-	
-TP_LOCALIZACAO_ESC	Localização (Escola)
-	
-TP_SIT_FUNC_ESC	Situação de funcionamento (Escola)
-	
-TP_LINGUA	Língua Estrangeira 
-Q001	Até que série seu pai, ou o homem responsável por você, estudou?
-	
-	
-	
-	
-	
-	
-	
-Q002	Até que série sua mãe, ou a mulher responsável por você, estudou?
-	
-	
-	
-	
-	
-	
-	
-Q003	A partir da apresentação de algumas ocupações divididas em grupos ordenados, indique o grupo que contempla a ocupação mais próxima da ocupação do seu pai ou do homem responsável por você. (Se ele não estiver trabalhando, escolha uma ocupação pensando no último trabalho dele).
-	
-	
-	
-	
-	
-Q004	A partir da apresentação de algumas ocupações divididas em grupos ordenados, indique o grupo que contempla a ocupação mais próxima da ocupação da sua mãe ou da mulher responsável por você. (Se ela não estiver trabalhando, escolha uma ocupação pensando no último trabalho dela).
-	
-	
-	
-	
-	
-Q005	Incluindo você, quantas pessoas moram atualmente em sua residência?
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-Q006	Qual é a renda mensal de sua família? (Some a sua renda com a dos seus familiares.)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-Q007	Em sua residência trabalha empregado(a) doméstico(a)?
-	
-	
-	
-Q008	Na sua residência tem banheiro?
-	
-	
-	
-	
-Q009	Na sua residência tem quartos para dormir?
-	
-	
-	
-	
-Q010	Na sua residência tem carro?
-	
-	
-	
-	
-Q011	Na sua residência tem motocicleta?
-	
-	
-	
-	
-Q012	Na sua residência tem geladeira?
-	
-	
-	
-	
-Q013	Na sua residência tem freezer (independente ou segunda porta da geladeira)?
-	
-	
-	
-	
-Q014	Na sua residência tem máquina de lavar roupa? (o tanquinho NÃO deve ser considerado)
-	
-	
-	
-	
-Q015	Na sua residência tem máquina de secar roupa (independente ou em conjunto com a máquina de lavar roupa)?
-	
-	
-	
-	
-Q016	Na sua residência tem forno micro-ondas?
-	
-	
-	
-	
-Q017	Na sua residência tem máquina de lavar louça?
-	
-	
-	
-	
-Q018	Na sua residência tem aspirador de pó?
-	
-Q019	Na sua residência tem televisão em cores?
-	
-	
-	
-	
-Q020	Na sua residência tem aparelho de DVD?
-	
-Q021	Na sua residência tem TV por assinatura?
-	
-Q022	Na sua residência tem telefone celular?
-	
-	
-	
-	
-Q023	Na sua residência tem telefone fixo?
-	
-Q024	Na sua residência tem computador?
-	
-	
-	
-	
-Q025	Na sua residência tem acesso à Internet?
+'Q001': escolaridade_pai,
+
+'Q002': escolaridade_mae,
+
+'Q003':	ocupacao_pai,
+
+'Q004':	ocupacao_mae,
+	
+'Q005':	pessoas_cohab,
+		
+'Q006':	renda_familiar,
+
+'Q007':	empregada_dom,
+
+'Q008':	num_banheiros,
+
+'Q009':	num_quartos,
+
+'Q010':	num_carro,
+
+'Q011':	num_motocicleta,
+	
+'Q012':	num_geladeira,
+
+'Q013':	num_freezer,
+
+'Q014':	num_maqlavar,
+	
+'Q015':	num_maqsecar,
+
+'Q016':	num_microondas,
+
+'Q017':	num_lavalouca,
+
+'Q018':	aspirador,
+	
+'Q019':	num_televisao,
+	
+'Q020':	dvd,
+	
+'Q021':	tv_assinatura,
+	
+'Q022':	num_celular,
+
+'Q023':	tel_fixo,
+	
+'Q024':	num_computador,
+
+'Q025':	acesso_internet
 		
 }
     
