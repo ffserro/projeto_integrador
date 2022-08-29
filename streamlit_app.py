4a9a6291d448184e0ee6e7e0b821be1cfc091e47
ffserro <ffserro@hotmail.com>
2022-08-29 16:45:08 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import lightgbm as lgb
+import matplotlib.pyplot as plt
 
 st.title('Projeto integrador')
 
 lgb_model = lgb.Booster(model_file='./LightGBMModel.txt')
 
-st.pyplot(lgb.plot_importance(lgb_model))
+fig, ax = plt.subplots(1,1)
+lgb.plot_importance(lgb_model, ax=ax)
+
+st.pyplot(fig)
