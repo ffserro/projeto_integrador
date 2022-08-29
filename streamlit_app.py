b936d039a63b8f0ea31e1a19bd87b9913f93aebc
ffserro <ffserro@hotmail.com>
2022-08-29 17:06:47 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import lightgbm as lgb
 import matplotlib.pyplot as plt
 
-st.markdown('# Digital House <img src="https://scontent.fsdu13-1.fna.fbcdn.net/v/t39.30808-6/275930420_1550645198655229_6570428577173776959_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=f28DmQAoRYEAX-DYGY0&_nc_ht=scontent.fsdu13-1.fna&oh=00_AT_G6wfELkAt7dyckIihVED_xXU3vF_iPffZPOZy-LkJxg&oe=6312AE5D" alt="Digital House" style="display:block;margin-left:auto;margin-right:auto;width:10%">', unsafe_allow_html=True)
+st.markdown('# Digital House <img src="https://scontent.fsdu13-1.fna.fbcdn.net/v/t39.30808-6/275930420_1550645198655229_6570428577173776959_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=f28DmQAoRYEAX-DYGY0&_nc_ht=scontent.fsdu13-1.fna&oh=00_AT_G6wfELkAt7dyckIihVED_xXU3vF_iPffZPOZy-LkJxg&oe=6312AE5D" alt="Digital House" style="display:inline-block;margin-left:auto;margin-right:auto;width:10%">', unsafe_allow_html=True)
 
 st.write('# Digital House')
 st.write('## Projeto integrador II')
